ce7f2e9bab99aa018051a3d1811cfcd02501125d
Fitwi-Gebray <fitwigebray8@gmail.com>
2024-12-21 18:14:57 +0100

    added model and to db

---
+++ backend/model/userModel.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    admin: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Hash password before saving
+userSchema.pre("save", async (next) => {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,28 @@
-const express = require('express');
-const cors = require('cors')
-const bodyParser = require('body-parser');
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const connectDB = require("./config/DB");
+const userModel = require("./model/userModel");
 
-
-const userRoutes = require('./routes/routes');
+const userRoutes = require("./routes/routes");
 
 const app = express();
-
 
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors())
+app.use(cors());
 
 app.use(userRoutes);
+app.use(userModel);
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, function () {
-  console.log(`Listening port ${PORT}`)
+  console.log(`Listening port ${PORT}`);
 });
 
-  module.exports = app
+module.exports = app;

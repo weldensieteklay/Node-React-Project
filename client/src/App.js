a94d7a2f126d3519e6b0daf39055d85c26b35773
Dejen Embaye <dejenteklay26@gmail.com>
2024-12-22 22:45:18 -0500

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,31 @@
 import React, { useState } from 'react';
 import {
-  CssBaseline,
+  // CssBaseline,
   AppBar,
   Toolbar,
   Typography,
   Box,
-  ThemeProvider,
+  // ThemeProvider,
 } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 import Profile from './components/common/Profile';
 import ErrorBoundary from './components/common/ErrorBoundary';
 import HomePage from './components/HomePage';
-
+import MenuIcon from '@mui/icons-material/Menu';
 import { useAuth } from './hooks/AuthContext';
+import Menu from './routes/Menu';
 
 const App = () => {
   const { logout, user } = useAuth(); 
+  const [menuOpen , setMenuOpen] = useState(false)
 
   const handleLogout = () => {
     logout();
   };
 
+  const toggleMenu = ()=>{
+     setMenuOpen(!menuOpen)
+  }
 
 
   return (
@@ -51,6 +56,13 @@ const App = () => {
                 <a href="/">
                   <HomeIcon style={{ cursor: 'pointer', color: 'white' }} />
                 </a>
+                {user?.token && (
+                       <MenuIcon 
+                          onClick={toggleMenu}
+                          style={{ cursor: 'pointer', marginLeft: '16px' }}
+                        />
+                  
+                )}
               </Box>
               {user?.token && (
                 <>
@@ -67,9 +79,12 @@ const App = () => {
                     />
                   </Box>
                 </>
-              )}
+              )} 
             </Toolbar>
           </AppBar>
+          {user?. token && (
+          <Menu open={menuOpen} onClose={toggleMenu} data={user}/>
+          )}
           {!user?.token ? (
             <Box style={{ flex: 1, overflow: 'auto' }}>
               <HomePage />
--- client/src/routes/routesConfig.js
+++ client/src/routes/routesConfig.js
@@ -1,27 +1,13 @@
-import Logout from '../components/common/Logout';
+
 import Profile from '../components/common/Profile';
-import Users from '../components/Users/Users'
-import AddBalance from '../components/purchases/Add-Balance'
-import Purchases from '../components/purchases/Purchases';
-import Calls from '../components/call/CallHistory';
+
 const routesConfig = [
+ 
     {
-        path: '/add-balance',
-        component: AddBalance,
-        label: 'Add Balance',
-        group: ['Add Balance']
-    },
-    {
-        path: '/purchases',
-        component: Purchases,
-        label: 'Purchases',
-        group: ['Purchases']
-    },
-    {
-        path: '/call',
-        component: Calls,
-        label: 'Calls',
-        group: ['Calls']
+        path: '/profile',
+        component: Profile,
+        label: 'Profile',
+        group: ['authentication']
     },
     {
         path: '/profile',
@@ -30,16 +16,16 @@ const routesConfig = [
         group: ['authentication']
     },
     {
-        path: '/logout',
-        component: Logout,
-        label: 'Logout',
-        group: ['authentication']
+        path: '/profile',
+        component: Profile,
+        label: 'Profile',
+        group: ['Items']
     },
     {
-        path: '/users',
-        component: Users,
-        label: 'Users',
-        group: ['admin']
+        path: '/profile',
+        component: Profile,
+        label: 'Profile',
+        group: ['Products']
     },
 ];
 

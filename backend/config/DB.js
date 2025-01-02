f3a9084356da9631833ec73db3a0508e46a022a1
Fitwi-Gebray <fitwigebray8@gmail.com>
2025-01-02 17:37:10 +0100

    new changes has been done

---
+++ backend/config/DB.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+// PGil1D8LN64KARDd
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(
+      `console.log('Connection to MongoDB established succesfully!'): ${conn.connection.host}`
+    );
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;

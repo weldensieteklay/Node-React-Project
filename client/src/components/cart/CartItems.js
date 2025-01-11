6906376d64e75f096ef872af692155d5dd0c7b38
DejenEmbaye <dejenteklay26@gmail.com>
2025-01-11 12:11:20 -0500

    css added

--- client/src/components/cart/CartItems.js
+++ client/src/components/cart/CartItems.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import CartItem from "./CartItem"
+import React from "react";
+import { useSelector } from "react-redux";
+import CartItem from "./CartItem";
 
 export const CartItems = () => {
-  const cartItems = useSelector(state=>state.cart.items)
-  const cartTotal = cartItems.reduce((total, item)=>total + item.quantity * item.price,0)
+  const cartItems = useSelector((state) => state.cart.items);
+  const cartTotal = cartItems.reduce(
+    (total, item) => total + item.quantity * item.price,
+    0
+  );
 
   return (
     <div>
-      <h1>Cart</h1>
-     <ul className='cartItems' >
-      {cartItems?.length > 0 && cartItems.map( item =><CartItem item={item}/>)}
-      {cartTotal }
-     </ul>
-
+      <h1 style={{ textAlign: "center" }}>Cart</h1>
+      <ul className="cartItems">
+        {cartItems?.length > 0 &&
+          cartItems.map((item) => <CartItem item={item} />)}
+        {cartTotal}
+      </ul>
     </div>
   );
 };
 
-export default CartItems ;
+export default CartItems;
